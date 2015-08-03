048d4242fe815bdeb20d9c2e97b757b07d83fd04
quartorz <tsuhaso@gmail.com>
2015-08-04 03:42:25 +0900

    add BCL_HAS_FUNCTION

--- include/bcl/has_xxx.hpp
+++ include/bcl/has_xxx.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <utility>
+#include <type_traits>
 
 namespace bcl{
 	namespace detail{
@@ -18,4 +19,17 @@ namespace bcl{
 	template <typename T>
 	constexpr auto has_value_v = has_value<T>::value;
 }
+
+// check if std::declval<T>().name(std::declval<U>()...) is well-defined
+#define BCL_HAS_FUNCTION(name)\
+	namespace detail{\
+		template <typename T, typename ... U>\
+		auto test_ ## name ## _func(int)\
+			-> decltype(::std::declval<T>().name(::std::declval<U>()...), void(0), ::std::true_type{});\
+		template <typename T, typename ... U>\
+		::std::false_type test_ ## name ## _func(long);\
+	}\
+	template <typename T, typename ... Args>\
+	class has_ ## name ## _func : decltype(detail::test_ ## name ## _func<T, Args...>()){\
+	};\
 
